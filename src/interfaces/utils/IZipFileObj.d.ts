6bdde25faeedb5ac3314ab8e02642bcbd2711801
MahtHdz <miguel.hztapia@outlook.com>
2025-01-06 21:58:09 -0600

    chore: :fire: remove files
    The unused files were removed.

--- src/interfaces/utils/IZipFileObj.d.ts
+++
@@ -1,9 +0,0 @@
-// import fileTypes from "@storage-api/config/static/fileTypes.json"
-import allowedFileTypes from "@storage-api/constants/fileTypes";
-import { PathLike } from "fs";
-
-export default interface IZipFileObj {
-  filepath: PathLike;
-  destination: PathLike;
-  type: keyof typeof allowedFileTypes;
-}
