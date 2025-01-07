39ac5c9efab5b61aa375508e932f63af1356b599
MahtHdz <miguel.hztapia@outlook.com>
2025-01-06 22:03:18 -0600

    feat: :sparkles: add controller
    The getMany controller was added.

--- src/controllers/get/index.ts
+++ src/controllers/get/index.ts
@@ -1,9 +1,13 @@
 import getOne from "./getOne.controller";
 import getAll from "./getAll.controller";
+import getMany from "./getMany.controller";
+import getOneWithFilter from "./getOneWithFilter.controller";
 
 const getControllers = {
   getOne,
   getAll,
+  getMany,
+  getOneWithFilter,
 };
 
 export default getControllers;
